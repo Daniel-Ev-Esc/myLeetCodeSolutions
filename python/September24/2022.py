b9490ae079eab876cc289e02a2e8f74e5794e347
Daniel Escalera <a00831289@tec.mx>
2024-09-01 14:50:34 -0600

    Divide by months

--- python/September24/2022.py
+++ python/September24/2022.py
@@ -1,3 +1,7 @@
+# 01/09/24
+# 2022. Convert 1D Array Into 2D Array
+# https://leetcode.com/problems/convert-1d-array-into-2d-array/description/
+
 class Solution(object):
     def construct2DArray(self, original, m, n):
         """
@@ -6,10 +10,11 @@ class Solution(object):
         :type n: int
         :rtype: List[List[int]]
         """
-
+        # A better way to do this is m*n == len(original) but i want to keep the solution as i got it
         if m*n < len(original) or len(original)/n < m:
             return []
-
+        
+        # The main idea is to build the subarrays and add them to the main array whrn n elements are inserted
         twoDimensionalArray = []
         internalList = []
 
@@ -20,6 +25,7 @@ class Solution(object):
             j+=1
             if j == n:
                 twoDimensionalArray.append(internalList)
+                # Reset the pointer and subarray
                 internalList = []
                 j=0
 
